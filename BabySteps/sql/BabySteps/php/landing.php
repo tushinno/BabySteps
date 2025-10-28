442b97eec554bfdaa8dbde4b949c7b9757603861
tushinno <rotsendeguzman72@gmail.com>
2025-10-29 05:10:55 +0800

    Delete BabySteps/sql/BabySteps directory

--- BabySteps/sql/BabySteps/php/landing.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/check_session.php';
-
-if (isset($_SESSION['user_id'])) {
-    header("Location: ../php/main.php");
-    exit;
-}
-
-include __DIR__ . '/../html/landing.php';
